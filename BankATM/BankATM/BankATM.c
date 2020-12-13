67ea2369c89217cdc939b31b3e519b8470cd128f
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-13 14:38:00 +0100

    done

---
+++ BankATM/BankATM/BankATM.c
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int moneySizes[12] = { 20000, 10000, 5000, 2000, 1000, 500,
+					200, 100, 50, 20, 10, 5 };
+
+	int change;
+	printf_s("How much is the change?\n");
+	scanf_s("%d", &change);
+
+	for (int i = 0; i < 12; i++)
+	{
+		int amountOfCurrentBill = change / moneySizes[i];
+		if (amountOfCurrentBill > 0) 
+		{
+			printf_s("%d X %d Forint\n", amountOfCurrentBill, moneySizes[i]);
+			change -= (amountOfCurrentBill * moneySizes[i]);
+
+		}
+	}
+	printf_s("\n");
+
+	return 0;
+}
